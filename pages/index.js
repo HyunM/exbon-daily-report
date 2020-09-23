72f8d892234e0473fb89ac1f81fe1d93cfb316fe
HyunM <wisek0226@gmail.com>
2020-09-23 13:02:43 -0700

    readme edit

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,67 @@
-import useSwr from 'swr'
-import Link from 'next/link'
+import { useState } from "react";
+import Link from "next/link";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import { Typography } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
 
-const fetcher = (url) => fetch(url).then((res) => res.json())
-
-export default function Index() {
-  const { data, error } = useSwr('/api/users', fetcher)
-
-  if (error) return <div>Failed to load users</div>
-  if (!data) return <div>Loading...</div>
+const Index = () => {
+  const [content, setContent] = useState(0);
+  const handleChange = (event, newValue) => {
+    setContent(newValue);
+  };
 
   return (
-    <ul>
-      {data.map((user) => (
-        <li key={user.id}>
-          <Link href="/user/[id]" as={`/user/${user.id}`}>
-            <a>{`User ${user.id}`}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  )
-}
+    <>
+      <AppBar position="static">
+        <Tabs value={content} onChange={handleChange} aria-label="tabs">
+          <Tab label="Timesheet" {...a11yProps(0)} />
+          <Tab label="Self-Perforemd Tasks" {...a11yProps(1)} />
+          <Tab label="Subcontractor Tasks" {...a11yProps(2)} />
+          <Tab label="Not Fixed" {...a11yProps(3)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={content} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={content} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={content} index={2}>
+        Item Three
+      </TabPanel>
+      <TabPanel value={content} index={3}>
+        Item Four
+      </TabPanel>
+    </>
+  );
+};
+
+const TabPanel = props => {
+  const { children, value, index, ...other } = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={4}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+};
+
+const a11yProps = index => {
+  return {
+    id: `tab-${index}`,
+    "aria-controls": `tabpanel-${index}`,
+  };
+};
+
+export default Index;
