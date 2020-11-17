b2c9e56be25ba08e032004e8e290f47231221b4e
HyunM <wisek0226@gmail.com>
2020-11-17 07:34:21 -0800

    deleted useless console

--- pages/api/timesheets.js
+++ pages/api/timesheets.js
@@ -55,7 +55,6 @@ const timesheetHandler = (req, res) => {
               console.error(err);
               return resolve();
             }
-            console.log(recordset);
             res.status(200).json({
               message: "Success, the timesheet has been created.",
               TimesheetID: recordset.recordset[0].TimesheetID,
