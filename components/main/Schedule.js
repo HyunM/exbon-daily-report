c0042bc7b9bea1e0cd160c397189e7420a7d92e7
HyunM <wisek0226@gmail.com>
2020-11-19 13:51:58 -0800

    calendar data loaded

--- components/main/Schedule.js
+++ components/main/Schedule.js
@@ -1,11 +1,89 @@
+import { useState, useMemo, useEffect } from "react";
 import styles from "./Schedule.module.css";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-
+import ReactTooltip from "react-tooltip";
+import { usePromiseTracker, trackPromise } from "react-promise-tracker";
+import Loader from "react-loader-spinner";
+import axios from "axios";
 const Schedule = () => {
+  const [data, setData] = useState(() => [
+    // {
+    //   title: "1",
+    //   start: "2020-11-10",
+    //   end: "2020-11-11",
+    // },
+    // {
+    //   title: "1",
+    //   start: "2020-11-10",
+    //   end: "2020-11-11",
+    // },
+    // {
+    //   title: "1",
+    //   start: "2020-11-10",
+    //   end: "2020-11-11",
+    // },
+    // {
+    //   title: "1",
+    //   start: "2020-11-10",
+    //   end: "2020-11-11",
+    // },
+    // {
+    //   title: "1",
+    //   start: "2020-11-10",
+    //   end: "2020-11-11",
+    // },
+    // {
+    //   title: "1",
+    //   start: "2020-11-10",
+    //   end: "2020-11-11",
+    // },
+  ]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let result = await axios({
+        method: "get",
+        url: `/api/dispatch-employee-assignment`,
+        timeout: 15000, // 15 seconds timeout
+        headers: {},
+      });
+      setData(result.data);
+      console.log(data);
+    };
+    trackPromise(fetchData());
+  }, []);
+
+  const { promiseInProgress } = usePromiseTracker();
   return (
     <div className={styles["frame"]}>
-      <FullCalendar plugins={[dayGridPlugin]} />
+      {promiseInProgress ? (
+        <div
+          style={{
+            width: "100%",
+            height: "100",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Loader type="ThreeDots" color="#2BAD60" height="100" width="100" />
+        </div>
+      ) : (
+        <>
+          <FullCalendar
+            plugins={[dayGridPlugin]}
+            height="750px"
+            initialView="dayGridMonth"
+            eventColor="#3f83c2"
+            events={data}
+            dayMaxEventRows={3}
+            eventTextColor="white"
+            displayEventTime={false}
+          />
+          {/* <ReactTooltip multiline={true} type="info" offset={{ right: 150 }} /> */}
+        </>
+      )}
     </div>
   );
 };
---
+++ pages/api/dispatch-employee-assignment.js
@@ -0,0 +1,52 @@
+const mssql = require("mssql");
+const dbserver = require("../../dbConfig.js");
+
+const dispatchEmployeeAssignmentHandler = (req, res) => {
+  const { method, body } = req;
+  return new Promise(resolve => {
+    switch (method) {
+      case "GET":
+        mssql.connect(dbserver.dbConfig, err => {
+          if (err) {
+            console.error(err);
+            return resolve();
+          }
+          const request = new mssql.Request();
+
+          const query = `SELECT DEA.[EmployeeID]
+                        ,E.LastName + ' ' + E.FirstName as 'EmployeeName'
+                        ,DEA.[ProjectID] as 'id'
+                        ,DP.Name as 'title'
+                        ,DATEADD(hour, 12, CONVERT(datetime, DEA.[StartDate])) as 'start'
+                        ,DATEADD(hour, 12, CONVERT(datetime, DEA.[EndDate])) as 'end'
+
+                        FROM [Exbon].[dbo].[DispatchEmployeeAssignment] DEA with(nolock)
+                        
+                        INNER JOIN [Exbon].[dbo].[DispatchProject] DP with(nolock) 
+                                ON DEA.ProjectID = DP.ID
+
+                        INNER JOIN [Exbon].[dbo].[Employee] E with(nolock)
+                                ON DEA.EmployeeID = E.EmployeeID`;
+
+          request.query(query, (err, recordset) => {
+            if (err) {
+              console.error(err);
+              return resolve();
+            }
+            const response = recordset.recordset;
+            res.status(200).json(response);
+            return resolve();
+          });
+        });
+        break;
+
+      default:
+        res.setHeader("Allow", ["GET"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+        res.status(404).end(`Failed`);
+        resolve();
+    }
+  });
+};
+
+export default dispatchEmployeeAssignmentHandler;
