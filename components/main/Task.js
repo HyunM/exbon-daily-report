4f1cd9d806744ea4618966be8bc1c08aedff1f08
HyunM <wisek0226@gmail.com>
2021-01-05 07:34:55 -0800

    wording change Approval in Completed

--- components/main/Task.js
+++ components/main/Task.js
@@ -1089,7 +1089,7 @@ const Task = ({
                     </thead>
                     <tbody>
                       {noWork.map((item) => {
-                        return item.Status === "Approval" ? (
+                        return item.Status === "Completed" ? (
                           <tr key={noWorkMapKey++}>
                             <td>{item.RecordID > 0 ? item.RecordID : ""}</td>
                             <td
@@ -1099,7 +1099,7 @@ const Task = ({
                                 ]
                               }
                             >
-                              Approval
+                              Completed
                             </td>
                             <td>
                               {formatDate(item.StartDate)} ~{" "}
