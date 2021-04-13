888390d22fccae6532000b2a588ad32f0feb2f50
HyunM <wisek0226@gmail.com>
2021-04-13 16:44:28 -0700

    test complete

--- pages/api/test.js
+++ pages/api/test.js
@@ -3,45 +3,22 @@ const testHandler = (req, res) => {
   return new Promise(resolve => {
     switch (method) {
       case "POST":
-        var Excel = require("exceljs");
-        var workbook = new Excel.Workbook();
-        var __dirname = "public";
-        workbook.xlsx.readFile(__dirname + "/test.xlsx").then(function () {
-          var worksheet = workbook.getWorksheet(1);
-          var row = worksheet.getRow(5);
-          row.getCell(1).value = 5; // A5's value set to 5
+        const handle = async () => {
+          var Excel = require("exceljs");
+          var __dirname = "public";
+          let filename = "/6 Daily Report.xlsx";
+          let workbook = new Excel.Workbook();
+          await workbook.xlsx.readFile(__dirname + filename);
+          let worksheet = workbook.getWorksheet("Daily Report");
+          // header id name dob
+          let row = worksheet.getRow(2);
+          console.log(row);
+          row.getCell(2).value = body.ProjectName;
           row.commit();
-          return workbook.xlsx.writeFile("new.xlsx");
-        });
-
-      // case "POST":
-      //   // Requiring module
-      //   const reader = require("xlsx");
-
-      //   // Reading our test file
-      //   const __dirname = "public";
-      //   const file = reader.readFile(__dirname + "/test.xlsx");
-
-      //   // Sample data set
-      //   let student_data = [
-      //     {
-      //       Student: "Nikhil",
-      //       Age: 22,
-      //       Branch: "ISE",
-      //       Marks: 70,
-      //     },
-      //     {
-      //       Name: "Amitha",
-      //       Age: 21,
-      //       Branch: "EC",
-      //       Marks: 80,
-      //     },
-      //   ];
-
-      //   reader.utils.book_append_sheet(file, ws, "TEST");
-
-      //   // Writing to our file
-      //   reader.writeFile(file, __dirname + "/test2.xlsx");
+          workbook.xlsx.writeFile(__dirname + "/6 Daily Report - Test.xlsx");
+        };
+        handle();
+        break;
 
       default:
         res.setHeader("Allow", ["POST"]);
--- pages/test.js
+++ pages/test.js
@@ -2,24 +2,33 @@ import axios from "axios";
 
 const test = () => {
   const inNode = () => {
-    let ProjectName = "TEST";
+    let ProjectName = document.getElementById("testinput").value;
+    console.log(ProjectName);
     axios({
-      method: "post",
+      method: "POST",
       url: `/api/test`,
-      timeout: 1000000, // 2 seconds timeout
+      timeout: 1000000,
       headers: {},
       data: {
         ProjectName: ProjectName,
       },
     });
+    alert("Edit Complete ! Click to download.");
   };
-  const inReact = () => {
-    let ProjectName = "TEST";
-  };
+
   return (
     <div>
-      <button onClick={inNode}>Nodejs EXCEL</button>
-      <button onClick={inReact}>React EXCEL</button>
+      Project Name : <input id="testinput"></input>
+      <br />
+      <br />
+      <br />
+      <button onClick={inNode}>EDIT EXCEL</button>
+      <br />
+      <br />
+      <br />
+      <a href="/6 Daily Report - Test.xlsx" download>
+        Click to download
+      </a>
     </div>
   );
 };
