2ea2af72df952aebb4827588eb151223f60fe327
HyunM <wisek0226@gmail.com>
2021-05-28 07:25:07 -0700

    overflow protected for Timesheet Task

--- pages/timesheet.js
+++ pages/timesheet.js
@@ -425,6 +425,16 @@ const Timesheet = () => {
               ></input>
             );
           }}
+          menuStyle={{
+            borderRadius: "3px",
+            boxShadow: "0 2px 12px rgba(0, 0, 0, 0.1)",
+            background: "rgba(255, 255, 255, 0.9)",
+            padding: "2px 0",
+            fontSize: "90%",
+            position: "fixed",
+            overflow: "auto",
+            maxHeight: "150px",
+          }}
         />
       );
     } else if (id === "Task") {
@@ -464,6 +474,16 @@ const Timesheet = () => {
                 {...props}
               ></input>
             );
+          }}
+          menuStyle={{
+            borderRadius: "3px",
+            boxShadow: "0 2px 12px rgba(0, 0, 0, 0.1)",
+            background: "rgba(255, 255, 255, 0.9)",
+            padding: "2px 0",
+            fontSize: "90%",
+            position: "fixed",
+            overflow: "auto",
+            maxHeight: "150px",
           }}
         />
       );
